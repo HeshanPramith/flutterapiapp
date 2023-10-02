21079d3cc03f4a1036ab1e8d1c5af15ab47062e4
Heshan Pramith <heshan@genesiis.com>
2023-10-02 20:32:31 +0530

    design extended v2

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyTheme {
-  static ThemeData darkTheme = ThemeData.dark();
-  static ThemeData lightTheme = ThemeData.light();
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+  );
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.green,
+  );
 }
